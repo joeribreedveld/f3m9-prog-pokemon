f15ebed46da96a72be2d16d90aa89d1e82320867
Duck1s <32653@ma-web.nl>
2022-10-07 14:18:13 +0200

    feat(pokemoncard): make pokemoncard, attack functions, catch function and add the buttons

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,12 +1,81 @@
 // Imports
+import React from "react"
 
 // Functions
-function PokemonCard() {
-	return (
-		<>
-			<h2>Pokemon Card</h2>
-		</>
-	)
+class PokemonCard extends React.Component {
+	// Constructor
+	constructor(props) {
+		super(props)
+		this.state = {
+			hp: 0,
+			caughtOrRan: false,
+		}
+	}
+
+	// Component Did Mount
+	componentDidMount() {
+		this.setState({
+			hp: this.props.pokemonHP,
+		})
+	}
+
+	// Attack
+	attack = () => {
+		let randomNumber = Math.floor(Math.random() * (31 - 1) + 1)
+		this.setState(
+			{
+				hp: this.state.hp - randomNumber,
+			},
+			function () {
+				if (this.state.hp <= 0) {
+					this.setState({
+						hp: 0,
+						caughtOrRan: true,
+					})
+				}
+			}
+		)
+	}
+
+	// Catch
+	catch = () => {
+		this.setState({ caughtOrRan: true })
+		let damagePercentage = 100 - Math.floor((this.state.hp / this.props.pokemonHP) * 100)
+		let dice = Math.floor(Math.random() * (100 - 1) + 1)
+		if (damagePercentage > dice) {
+			this.props.updateCounter()
+		}
+	}
+
+	// Render
+	render() {
+		// let types = this.props.pokemonType.map(function (type) {
+		// 	return (
+		// 		<span key={type} className={`pokemonCard__type pokemonCard__type--${type}`}>
+		// 			{type}
+		// 		</span>
+		// 	)
+		// })
+		let buttons = null
+		if (this.state.caughtOrRan === false) {
+			buttons = (
+				<>
+					<button onClick={this.attack} className=''>
+						Attack
+					</button>
+					<button onClick={this.catch} className=''>
+						Catch
+					</button>
+				</>
+			)
+		}
+
+		return (
+			<>
+				<h2>Pokemon Card</h2>
+			</>
+		)
+	}
 }
 
 // Exports
